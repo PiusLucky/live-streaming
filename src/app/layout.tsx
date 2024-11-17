2f954a52e2bbb648431a99d955c603a13ed92488
luckypius <luckypius50@gmail.com>
2024-11-17 20:41:00 +0100

    chore: zego sdk setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from "next/font/google"
 import './globals.css'
+import { cn } from '@/lib/utils'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,7 +18,7 @@ export default function RootLayout({
 }) {
   return (   
     <html lang="en">
-      <body  className={inter.className} >{children}</body>
+      <body  className={cn("h-screen w-screen", inter.className)} >{children}</body>
     </html>
   )
 }
---
+++ src/lib/constant.ts
@@ -0,0 +1,2 @@
+export const appID = "YOUR_ZEGO_APP_ID";
+export const serverSecret = "YOUR_ZEGO_SERVER_SECRET";
